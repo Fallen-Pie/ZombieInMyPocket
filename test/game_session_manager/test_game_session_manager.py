46e6cbc459765d8974329f2f689a6aaa7d2d1813
arsenie-sarmiento <111683775+arsenie-sarmiento@users.noreply.github.com>
2025-09-23 05:35:12 +1200

    added tests

--- test/game_session_manager/test_game_session_manager.py
+++ test/game_session_manager/test_game_session_manager.py
@@ -1,6 +1,33 @@
-import unittest
+from unittest.mock import Mock
 from src.model.game_session_manager import GameSessionManager
 from src.enums_and_types.game_state import GameState
+
+def test_game_over_when_hp_zero():
+    player = Mock(hp=0)
+    time = Mock(is_midnight=lambda: False)
+    turn = Mock(number=1)
+    totem = Mock(is_buried=False)
+    messenger = Mock()
+
+    session = GameSessionManager(player, time, turn, totem, messenger)
+
+    assert session.is_game_over() is True
+
+
+def test_game_snapshot_structure():
+    player = Mock(hp=5, reset=lambda: None)
+    time = Mock(is_midnight=lambda: False, __str__=lambda s: "11:00pm", reset=lambda: None)
+    turn = Mock(number=2, reset=lambda: None)
+    totem = Mock(is_buried=False, reset=lambda: None)
+    messenger = Mock(clear_messages=lambda: None)
+
+    session = GameSessionManager(player, time, turn, totem, messenger)
+    snap = session.get_snapshot()
+
+    assert snap["hp"] == 5
+    assert snap["time"] == "11:00pm"
+    assert snap["turn"] == 2
+    assert snap["totem_buried"] is False
 
 # def test_initial_state(manager):
 #     assert manager.get_current_state() == GameState.INIT
